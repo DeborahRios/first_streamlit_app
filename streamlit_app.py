e7a46b1aff3254e54230046b0bee76176fc1bd56
DeborahRios <43423330+DeborahRios@users.noreply.github.com>
2023-08-12 20:19:49 -0400

    Update streamlit_app.py
    fix error typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import  streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites 🥣 🥗 🐔 🥑🍞')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
