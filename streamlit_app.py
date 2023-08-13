c772e5f269e30b5f943a6b28704b48ddd52fc19f
DeborahRios <43423330+DeborahRios@users.noreply.github.com>
2023-08-12 20:29:46 -0400

    Update streamlit_app.py
    added pandas lib
    added pulling CSV data file into a pandas dataframe (Fruit Smoothie)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import  streamlit
+import pandas
+
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites 🥣 🥗 🐔 🥑🍞')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -6,4 +8,7 @@ streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocate Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
